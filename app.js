904dbbcc1cce5476f90a713bac460cdfaceb22fa
Harikrishnan <harikrishnankannoth@gmail.com>
2023-03-09 14:54:30 +0530

    Added Files

--- app.js
+++ app.js
@@ -1,10 +1,80 @@
 const express = require("express");
 const request = require("request");
-const bodyParser = require("bosy-parser");
+const bodyParser = require("body-parser");
+const https = require("https");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 
 const app = express();
 
-app.listen(3000,()=>{
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/signup.html");
+})
+
+//Setting up MailChimp
+mailchimp.setConfig({
+    //*****************************ENTER YOUR API KEY HERE******************************
+     apiKey: "fa0ee32c33e973be21d224ca75e184cc-us21",
+    //*****************************ENTER YOUR API KEY PREFIX HERE i.e.THE SERVER******************************
+     server: "us21"
+    });
+
+
+
+
+app.post("/", function (req,res) {
+    //*****************************CHANGE THIS ACCORDING TO THE VALUES YOU HAVE ENTERED IN THE INPUT ATTRIBUTE IN HTML******************************
+    const firstName = req.body.fName;
+    const secondName = req.body.lName;
+    const email = req.body.email;
+    //*****************************ENTER YOU LIST ID HERE******************************
+    const listId = "a29c5fb31f";
+    //Creating an object with the users data
+    const subscribingUser = {
+     firstName: firstName,
+     lastName: secondName,
+     email: email
+    };
+    //Uploading the data to the server
+     async function run() {
+    const response = await mailchimp.lists.addListMember(listId, {
+     email_address: subscribingUser.email,
+     status: "subscribed",
+     merge_fields: {
+     FNAME: subscribingUser.firstName,
+     LNAME: subscribingUser.lastName
+    }
+    });
+    //If all goes well logging the contact's id
+     res.sendFile(__dirname + "/success.html")
+     console.log(
+    `Successfully added contact as an audience member. The contact's id is ${
+     response.id
+     }.`
+    );
+    }
+    //Running the function and catching the errors (if any)
+    // ************************THIS IS THE CODE THAT NEEDS TO BE ADDED FOR THE NEXT LECTURE*************************
+    // So the catch statement is executed when there is an error so if anything goes wrong the code in the catch code is executed. In the catch block we're sending back the failure page. This means if anything goes wrong send the faliure page
+     run().catch(e => res.sendFile(__dirname + "/failure.html"));
+    });
+
+
+
+
+
+app.listen(process.env.PORT||3000,()=>{
     console.log("Server running on port 3000");
 })
+
+
+
+
+//APIKEY
+// fa0ee32c33e973be21d224ca75e184cc-us21
+
+//AUDIENCE ID
+// a29c5fb31f
